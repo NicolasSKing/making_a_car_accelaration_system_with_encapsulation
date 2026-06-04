fcc90b133187aa0ed6a8f5ff27cdf0d6aa528e65
NicolasSKing <nocoben10@gmail.com>
2026-06-04 11:02:59 +0800

    here_i_put_the_year_model_and_make_in_the_car_class_2024_and_toyota

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from car_class import Car
 
-my_car = Car()
+my_car = Car(2024, "TOYOTA")
 
 print("Accelerating:")
 for i in range(5):
     my_car.accelerate()
     print(f"Current Speed: {my_car.get_speed()}")
+
+
+
